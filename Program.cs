fcfa9a3cd3067682523235a172ff52fec30cdbaf
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2024-05-17 19:08:58 -0300

    my custom

--- Program.cs
+++ Program.cs
@@ -5,10 +5,11 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Welcome to JS parking !\n Enter the initial value: ");
+Console.WriteLine("Welcome to JS parking !");
+Console.Write("Enter the initial value: ");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Enter the price per hour: ");
+Console.Write("Enter the price per hour: ");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
@@ -20,11 +21,12 @@ bool exibirMenu = true;
 while (exibirMenu)
 {
     Console.Clear();
-    Console.WriteLine("Enter your option: ");
+    Console.WriteLine("<<< Menu JS Park >>>: ");
     Console.WriteLine("1 - Register vehicle");
     Console.WriteLine("2 - Remove vehicle");
     Console.WriteLine("3 - List vehicles");
     Console.WriteLine("4 - Close");
+    Console.Write("Enter your option: ");
 
     switch (Console.ReadLine())
     {
@@ -49,8 +51,8 @@ while (exibirMenu)
             break;
     }
 
-    Console.WriteLine("Press a key to continue");
-    Console.ReadLine();
+    // Console.WriteLine("Press a key to continue");
+    // Console.ReadLine();
 
 
 }
